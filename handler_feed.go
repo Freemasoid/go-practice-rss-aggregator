9a8a79ca8e5013bf91cf2f7db632cae0b60c335f
Freemasoid <freemasoid@icloud.com>
2024-04-23 22:16:08 +0200

    feed handler

---
+++ handler_feed.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Freemasoid/go-practice-rss-aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		Name string `json: "name"`
+		URL  string `json: "url"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("error parsing json: %v", err))
+	}
+
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+		Url:       params.URL,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("could not create feed: %v", err))
+	}
+
+	respondWithJSON(w, 201, databaseFeedToFeed(feed))
+}
