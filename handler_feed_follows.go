c022e67f470c33f2cdd1cbb65528aec96b59604b
Freemasoid <freemasoid@icloud.com>
2024-04-24 22:04:25 +0200

    get followed feeds

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -37,3 +37,13 @@ func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.
 
 	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
 }
+
+func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Request, user database.User) {
+
+	feedFollows, err := apiCfg.DB.GetFeedFollows(r.Context(), user.ID)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("could not get feed follows: %v", err))
+	}
+
+	respondWithJSON(w, 201, databaseFeedFollowsToFeedFollows(feedFollows))
+}
