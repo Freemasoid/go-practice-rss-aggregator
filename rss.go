bfeb537930d32046c555db8dcc88f1b947e38bbc
Freemasoid <freemasoid@icloud.com>
2024-04-28 23:57:36 +0200

    fetch feed update overtime

---
+++ rss.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Language    string    `xml:"Language"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func urlToFeed(url string) (RSSFeed, error) {
+	httpClient := http.Client{
+		Timeout: 10 * time.Second,
+	}
+}
